0be0929dd5e81dfd4526aa1ef239520160da5b6d
whiteadol <whiteadol3@naver.com>
2023-04-24 09:04:14 +0900

    - 테스트용

---
+++ src/main/java/com/example/webpackdemo/WebpackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webpackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpackDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebpackDemoApplication.class, args);
+    }
+
+}
