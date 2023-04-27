d0a40620b1fc85340a76f234a92e3d205f3999ad
jhpark <hotkk22@naver.com>
2023-04-27 10:27:02 +0900

    박주현 - 컨벤션 추가

---
+++ src/main/resources/static/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	singleQuote: true,
+	semi: true,
+	useTabs: true,
+	tabWidth: 4,
+	trailingComma: 'all',
+	printWidth: 200,
+	arrowParens: 'avoid',
+};
--- src/main/resources/static/js/business/fireDetect.js
+++ src/main/resources/static/js/business/fireDetect.js
@@ -1,16 +1,13 @@
-'use strict';
+"use strict";
 
-import { Ws } from '../util/wsUtil.js';
-import { Loading, Notify } from 'notiflix';
-import { ContextMenu, SkinMenu } from '../lib/context.js';
-import { defMenu } from '../util/context/defMenu.js';
+import { Ws } from "../util/wsUtil.js";
+import { Loading, Notify } from "notiflix";
+import { ContextMenu, SkinMenu } from "../lib/context.js";
+import { defMenu } from "../util/context/defMenu.js";
+import { IsotopeCntr } from "../util/componentCntr.js";
+const axios = require("axios");
 
-import { IsotopeCntr } from '../util/componentCntr.js';
-const axios = require('axios');
-
-
-document.addEventListener('DOMContentLoaded', async () => {
-
+document.addEventListener("DOMContentLoaded", async () => {
     const isopGrid = $(".grid").isotope({
         itemSelector: ".card",
         layoutMode: "fitRows",
@@ -18,7 +15,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         sortAscending: {
             date: true,
             temperate: true,
-            'card-title': true,
+            "card-title": true,
         },
         getSortData: {
             //온도 내림차순
@@ -32,65 +29,66 @@ document.addEventListener('DOMContentLoaded', async () => {
                 return parseFloat(date.replace(/[\(\)]/g, ""));
             },
             //구획명 내림차순
-            'card-title': function (itemElem) {
+            "card-title": function (itemElem) {
                 return $(itemElem).find(".card-title").text();
-            }
+            },
         },
     });
     const isotopeCntr = new IsotopeCntr({
         inst: isopGrid,
     });
-
     const itemList = [
         {
-            text: '전체 초기화',
+            text: "전체 초기화",
             onclick: e => {
-                isopGrid.isotope({ sortBy: 'original-order' });
-                isopGrid.isotope({ filter: '*' });
+                isopGrid.isotope({ sortBy: "original-order" });
+                isopGrid.isotope({ filter: "*" });
             },
         },
         {
-            text: '정렬 초기화',
+            text: "정렬 초기화",
             onclick: e => {
-                isopGrid.isotope({ sortBy: 'original-order' });
+                isopGrid.isotope({ sortBy: "original-order" });
             },
         },
         {
-            text: '필터 초기화',
+            text: "필터 초기화",
             onclick: e => {
-                isopGrid.isotope({ filter: '*' });
+                isopGrid.isotope({ filter: "*" });
             },
         },
     ];
-
-    await axios.get('/camera-api/getCameraEvent').then(res => {
-        res.data.forEach((camera, i) => {
-            const floor = Math.floor(i / 20);
-            const room = i % 20;
-            const floorStr = floor < 10 ? `${floor}` : `${floor}`;
-            const roomStr = room < 10 ? `0${room}` : `${room}`;
-            camera['floor'] = 1;
-            camera['floorName'] = `B${floorStr}F-${roomStr}`;
-            const appendElemnt = isotopeCntr.CreateIsotElmnt({
-                data: camera,
-                opt: {
-                    idx: i,
-                },
+    //NIFI Rest API - 카메라 이벤트 조회
+    await axios
+        .get("/camera-api/getCameraEvent")
+        .then(res => {
+            res.data.forEach((camera, i) => {
+                const floor = Math.floor(i / 20);
+                const room = i % 20;
+                const floorStr = floor < 10 ? `${floor}` : `${floor}`;
+                const roomStr = room < 10 ? `0${room}` : `${room}`;
+                camera["floor"] = 1;
+                camera["floorName"] = `B${floorStr}F-${roomStr}`;
+                const appendElemnt = isotopeCntr.CreateIsotElmnt({
+                    data: camera,
+                    opt: {
+                        idx: i,
+                    },
+                });
+                isotopeCntr.AppendIsotElmnt(appendElemnt);
             });
-            isotopeCntr.AppendIsotElmnt(appendElemnt);
+        })
+        .catch(err => {
+            console.log(err);
         });
-
-    }).catch(err => {
-        console.log(err);
-    });
 
     const skinMenu = new SkinMenu();
     const kaliContextMenu = new ContextMenu(document.body, itemList);
-    const subItemList = [...defMenu(isopGrid), { text: '데이터 필터', hotkey: '❯', submenu: kaliContextMenu }, { text: '스킨', hotkey: '❯', submenu: skinMenu.Init() }]
+    const subItemList = [...defMenu(isopGrid), { text: "데이터 필터", hotkey: "❯", submenu: kaliContextMenu }, { text: "스킨", hotkey: "❯", submenu: skinMenu.Init() }];
     const chromeContextMenu = new ContextMenu(document.body, subItemList);
     chromeContextMenu.install();
 
-    document.addEventListener('contextmenu', function (event) {
+    document.addEventListener("contextmenu", function (event) {
         event.preventDefault(); // 기본 동작 중단
         const [x, y] = [event.clientX, event.clientY];
         const element = document.elementFromPoint(x - 5, y - 5); // x, y 좌표에 해당하는 HTML 요소 가져오기
@@ -98,7 +96,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         //최상위
         if (cameraWrapCard) {
             const { cameraCh, cameraIp, regDt, tempAvg, tempCenter, tempMax, tempMin } = cameraWrapCard.dataset;
-            console.log(`cameraCh: ${cameraCh}, cameraIp: ${cameraIp}, regDt: ${regDt}, tempAvg: ${tempAvg}, tempCenter: ${tempCenter}, tempMax: ${tempMax}, tempMin: ${tempMin}`)
+            console.log(`cameraCh: ${cameraCh}, cameraIp: ${cameraIp}, regDt: ${regDt}, tempAvg: ${tempAvg}, tempCenter: ${tempCenter}, tempMax: ${tempMax}, tempMin: ${tempMin}`);
         } else {
             chromeContextMenu.hideAll();
         }
@@ -107,9 +105,9 @@ document.addEventListener('DOMContentLoaded', async () => {
     $("#filters").on("click", "button", function () {
         const filterValue = $(this).attr("data-filter");
         const floor = function () {
-            return this.querySelector(`div[name="floor"]`).innerHTML == filterValue
+            return this.querySelector(`div[name="floor"]`).innerHTML == filterValue;
         };
-        isopGrid.isotope({ filter: filterValue === '*' ? filterValue : floor });
+        isopGrid.isotope({ filter: filterValue === "*" ? filterValue : floor });
     });
     // bind sort button click
     $("#sorts").on("click", "button", function () {
@@ -129,7 +127,8 @@ document.addEventListener('DOMContentLoaded', async () => {
          isopGrid.isotope({ filter: test });
      }); */
 
-    setTimeout(() => $(isopGrid).isotope('layout'), 500);
+    //0.5초후 레이아웃 재정렬
+    setTimeout(() => $(isopGrid).isotope("layout"), 500);
 
     setTimeout(() => {
         //동적으로 정렬방식 변경
@@ -140,36 +139,35 @@ document.addEventListener('DOMContentLoaded', async () => {
         //console.log(isopGrid)
     }, 2000);
 
-    //const buffer = JsonToBinary(data); // JSON 데이터를 binary 데이터로 변환
+    //WebSocket 초기화
     const nifiSocket = new WebSocket(`wss://${window.location.hostname}/ws`);
-    nifiSocket.binaryType = 'arraybuffer';
-    nifiSocket.onopen = () => {
-        console.log('nifi 연결 성공');
-        /*  setInterval(() => {
-             nifiSocket.send({
-                 sendType: 'data',
-                 message: null,
-             });
-         }, 2000); */
-    };
+    nifiSocket.binaryType = "arraybuffer";
+    nifiSocket.onopen = () => console.log("nifi 연결 성공");
     nifiSocket.onmessage = event => {
         const cameraEvt = JSON.parse(new TextDecoder().decode(event.data));
         const { max_temp, min_temp, avg_temp, ip, port, rng_id } = cameraEvt;
-        console.log(max_temp)
+        console.log(max_temp);
         isopGrid.find(`.grid-item`).each(function () {
             if (this.id == `camera_${ip}_${port}_${rng_id}`) {
-                this.querySelector(`div[name="cBody"]`).style.transition = 'background-color 1s ease-in-out';
-                this.querySelector(`div[name="cBody"]`).style.backgroundColor = isotopeCntr.CardBackColor(max_temp);
-                this.querySelector(`div[name="temperate"]`).innerHTML = max_temp;
-                this.querySelector(`h7[name="viewTemp"]`).innerText = `${parseFloat(max_temp).toFixed(1)}℃`;
-                this.querySelector(`div[name="nowTempLine"]`).style.transition = 'top 1s ease-in-out';
-                this.querySelector(`div[name="nowTempLine"]`).style.top = `${100 - parseFloat(max_temp)}px`;
-                this.querySelector(`div[name="nowTempArrow"]`).style.transition = 'top 1s ease-in-out';
-                this.querySelector(`div[name="nowTempArrow"]`).style.top = `${100 - (parseFloat(max_temp) + 4)}px`;
+                const [cBody, temperate, viewTemp, nowTempLine, nowTempArrow] = [
+                    this.querySelector(`div[name="cBody"]`),
+                    this.querySelector(`div[name="temperate"]`),
+                    this.querySelector(`h7[name="viewTemp"]`),
+                    this.querySelector(`div[name="nowTempLine"]`),
+                    this.querySelector(`div[name="nowTempArrow"]`),
+                ];
+                cBody.style.transition = "background-color 1s ease-in-out";
+                cBody.style.backgroundColor = isotopeCntr.CardBackColor(max_temp);
+                temperate.innerHTML = max_temp;
+                viewTemp.innerText = `${parseFloat(max_temp).toFixed(1)}℃`;
+                nowTempLine.style.transition = "top 1s ease-in-out";
+                nowTempLine.style.top = `${100 - parseFloat(max_temp)}px`;
+                nowTempArrow.style.transition = "top 1s ease-in-out";
+                nowTempArrow.style.top = `${100 - (parseFloat(max_temp) + 4)}px`;
                 //isopGrid.isotope('updateSortData', this)
             }
         });
-        isopGrid.isotope('updateSortData').isotope();
+        isopGrid.isotope("updateSortData").isotope();
         /* cameraEvt.forEach((one) => {
             const { max_temp, min_temp, avg_temp, ip, port, rng_id } = one;
             console.log(one)
@@ -193,6 +191,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         console.log(error);
     };
     nifiSocket.onclose = () => {
-        console.log('nifi 연결 종료');
+        console.log("nifi 연결 종료");
     };
 });
